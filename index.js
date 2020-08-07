41ec2a1e7859b4e869dd87ee945bcbf39b835ab0
Bradley Fojas <43628838+bfojas@users.noreply.github.com>
2020-08-07 11:56:07 -0700

    working

---
+++ index.js
@@ -0,0 +1,37 @@
+const AWS = require("aws-sdk");
+const rekognition = new AWS.Rekognition();
+
+async function handler(event, context) {
+  console.log("EVENT BODY:", event.body);
+
+  var rekognitionParams = {
+    Image: {
+      S3Object: {
+        Bucket: process.env.BUCKET_NAME,
+        Name: event.body.fileName,
+      },
+    },
+    MaxLabels: 10,
+    MinConfidence: 70,
+  };
+
+  console.log("PARAMS:", rekognitionParams);
+
+  console.log("RUNNING REKOGNITION");
+  return new Promise((resolve, reject) => {
+    return rekognition.detectLabels(rekognitionParams, async (err, data) => {
+      if (err) {
+        console.log(err, err.stack);
+        return reject(err);
+      } else {
+        console.log("DATA LABELS:", data.Labels);
+        let hotdog = data.Labels.findIndex(val => val["Name"] === 'Hot Dog')
+        let result = hotdog >= 0 ? true : false
+
+        return resolve(result);
+      }
+    });
+  });
+}
+
+module.exports = { handler };
