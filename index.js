8fd465d06e33a6943af47fb54b4e704b05178f61
Bradley Fojas <43628838+bfojas@users.noreply.github.com>
2020-08-08 23:49:11 -0700

    refactor for querystring

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const AWS = require("aws-sdk");
 const rekognition = new AWS.Rekognition();
 
 async function handler(event, context) {
-  console.log("EVENT BODY:", event.body);
 
   var rekognitionParams = {
     Image: {
       S3Object: {
         Bucket: process.env.BUCKET_NAME,
-        Name: event.body.fileName,
+        Name: event['params']['querystring']['filename'],
       },
     },
     MaxLabels: 10,
     MinConfidence: 70,
   };
-
-  console.log("PARAMS:", rekognitionParams);
 
   console.log("RUNNING REKOGNITION");
   return new Promise((resolve, reject) => {
@@ -25,9 +22,8 @@ async function handler(event, context) {
         return reject(err);
       } else {
         console.log("DATA LABELS:", data.Labels);
-        let hotdog = data.Labels.findIndex(val => val["Name"] === 'Hot Dog')
-        let result = hotdog >= 0 ? true : false
-
+        let hotdog = data.Labels.findIndex(val => val["Name"] === 'Hot Dog');
+        let result = hotdog >= 0 ? true : false;
         return resolve(result);
       }
     });
